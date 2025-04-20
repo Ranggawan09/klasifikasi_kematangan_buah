2124e7c849cfcca0e77887a5c39fbaaa83b788d7
Ranggawan09 <ranggawan09@gmail.com>
2025-04-20 19:25:16 +0700

    v1

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 # Inisialisasi Roboflow Client
 CLIENT = InferenceHTTPClient(
-    api_url="https://detect.roboflow.com",
-    api_key="DEbxWv4B4sXByBVSaiaN"  # Gantilah dengan API Key-mu sendiri
+    api_url="https://serverless.roboflow.com",
+    api_key="DEbxWv4B4sXByBVSaiaN"
 )
 
 UPLOAD_FOLDER = "static/uploads"
@@ -35,15 +35,25 @@ def upload():
     file.save(filepath)
 
     # Kirim gambar ke Roboflow untuk analisis
-    model_id = "fruit-ripeness-ffuvb/1"
+    model_id="apple-ripeness-pj4d3/3"
     result = CLIENT.infer(filepath, model_id=model_id)
+
+    ripeness_translation = {
+        "100%_ripeness": "Tingkat Kematangan 100%",
+        "75%_ripeness": "Tingkat Kematangan 75%",
+        "50%_ripeness": "Tingkat Kematangan 50%",
+        "20%_ripeness": "Apel Masih Mentah",
+        "rotten_apple": "Apel Busuk"
+    }
 
     # Ambil hasil klasifikasi
     if "predictions" in result and len(result["predictions"]) > 0:
         prediction = result["predictions"][0]
-        ripeness = prediction.get("class", "Unknown")  # Nama tingkat kematangan buah
+        ripeness_en = prediction.get("class", "Tidak Diketahui")
+        ripeness = ripeness_translation.get(ripeness_en, "Tidak Diketahui")
+
     else:
-        ripeness = "Unknown"
+        ripeness = "Tidak Diketahui"
 
     # Kirim respons ke frontend
     return jsonify({"ripeness": ripeness, "image_url": filepath})
